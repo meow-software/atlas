a000db841f17d81f286581a09e400a4bdfb6c135
Au = Or[79]² <assanisaidelamani@gmail.com>
2025-08-31 03:12:49 +0200

    [ADD] - Responce formatter service decorators interceptor for convert bigint to string, and remove fields like secret, password

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -2,3 +2,4 @@ export type Snowflake = string;
 export * from "./common/tokens.utils";
 export * from "./common/event-bus.interface";
 export * from "./common/isnowflake-generator.interface";
+export * from "./common/response-formatter";
